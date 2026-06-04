56ce6340bce1039b53edf865002731ad30617669
Hoshuuch <jaygrittt@.gmail.com>
2026-06-04 20:48:11 +0800

    fix repository service startup wiring

--- src/main/java/com/youbi/monitor/MonitorBeApplication.java
+++ src/main/java/com/youbi/monitor/MonitorBeApplication.java
@@ -1,10 +1,11 @@
 package com.youbi.monitor;
 
+import org.apache.ibatis.annotations.Mapper;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@MapperScan("com.youbi.monitor.repository")
+@MapperScan(basePackages = "com.youbi.monitor.repository", annotationClass = Mapper.class)
 @SpringBootApplication
 public class MonitorBeApplication {
     public static void main(String[] args) {
